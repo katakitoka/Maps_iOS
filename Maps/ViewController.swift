7427e14482a41ecb22142fd512deeeb350d17e2b
Katakitoka <katakitoka116@gmail.com>
2016-05-03 13:10:16 +0300

    Creation

---
+++ Maps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Alina Cat on 03.05.16.
+//  Copyright © 2016 Alina Cat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
